e314397d3e3925a8f60e296e6215659a2a5bdc2b
ramazantufekci <ramazan.tufekci@outlook.com>
2019-04-25 10:04:25 +0300

    salt

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class DefaultController extends AbstractController
+ {
+     /**
+      * @Route("/hello/{name}")
+      */
+     public function index($name)
+     {
+
+        return $this->render('default/index.html.twig', [
+            'name' => $name,
+        ]);
+     }
+	 
+	 public function deneme()
+	 {
+		return $this->render('base.html.twig');
+	 }
+}
